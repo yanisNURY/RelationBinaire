22dd5b9c5784cc9faa8b0a99012673acdfd6b4d9
yanisNURY <115158095+yanisNURY@users.noreply.github.com>
2022-12-29 13:04:22 +0100

    29/12
    yanis

--- RelationBinaire.java
+++ RelationBinaire.java
@@ -10,10 +10,10 @@ public class RelationBinaire {
     public EE[] tabSucc;    // tableau des ensembles de successeurs
 
     // outils
-    public static int [][] copieMat (int t [][]){
-        int[][]tCop=new int[t.length][t[0].length];
+    public static boolean [][] copieMat (boolean t [][]){
+        boolean[][]tCop=new boolean[t.length][t[0].length];
         for(int i=0;i<t.length;i++){
-            for (intj=0;j<t[0].length;j++){
+            for (int j=0;j<t[0].length;j++){
                 tCop[i][j]=t[i][j];
             }
         }
@@ -118,224 +118,407 @@ public class RelationBinaire {
      action : construit une relation binaire dont la matrice d'adjacence
      est une copie de mat
      */
-    public RelationBinaire(int[][] mat){
+    public RelationBinaire(boolean[][] mat){
         this.n=mat.length;
         this.matAdj=copieMat(mat);
+        this.tabSucc=new EE[mat.length];
+        int card=0;
+        for (int i=0;i< mat.length;i++){
+            for (int j=0;j< mat.length;j++){
+                if(mat[i][j]) {
+                    this.tabSucc[i]=new EE(mat.length);
+                    this.tabSucc[i].ajoutElt(j);
+                    card++;
+                }
+            }
+        }
+        this.m=card;
+
+    }
+
+
+//    //______________________________________________
+
+
+    /** pré-requis : tab.length > 0 et pour tout i, les éléments de tab[i]
+     sont compris entre 0 et tab.length-1
+     action : construit une relation binaire dont le tableau des ensembles de successeurs
+     est une copie de tab
+     */
+    public RelationBinaire(EE[] tab){
+        this.tabSucc=new EE[tab.length];
+        for(int i=0;i<tab.length;i++){
+            this.tabSucc[i]=new EE(tab[i]);
+        }
+        int card=0;
+        this.n=tab.length;
+        this.matAdj=new boolean[tab.length][tab.length];
+        for(int i=0;i<tab.length;i++){
+            for (int j=0;j<tab.length;j++){
+                if(tabSucc[i].contient(j)){
+                    this.matAdj[i][j]=true;
+                    card++;
+                }
+                else this.matAdj[i][j]=false;
+            }
+        }
+        this.m=card;
     }
 
 //    //______________________________________________
-//
-//
-//    /** pré-requis : tab.length > 0 et pour tout i, les éléments de tab[i]
-//     sont compris entre 0 et tab.length-1
-//     action : construit une relation binaire dont le tableau des ensembles de successeurs
-//     est une copie de tab
-//     */
-//    public RelationBinaire(EE[] tab){
-//
-//    }
-//
+
+
+    /** pré-requis : aucun
+     action : construit une copie de r
+     */
+    public RelationBinaire(RelationBinaire r){
+        this.n=r.n;
+        this.m=r.m;
+        this.matAdj=copieMat(r.matAdj);
+        this.tabSucc=new EE[r.n];
+        for(int i=0;i< r.n;i++){
+            this.tabSucc[i]=new EE(r.tabSucc[i]);
+        }
+    }
+
+
 //    //______________________________________________
-//
-//
-//    /** pré-requis : aucun
-//     action : construit une copie de r
-//     */
-//    public RelationBinaire(RelationBinaire r){
-//
-//    }
-//
-//
-//    //______________________________________________
-//
-//
-//    // méthodes
-//
-//
-//    /** pré-requis : aucun
-//     résultat : une chaîne de caractères permettant d'afficher this par sa matrice d'adjacence
-//     contenant des '0' et des '1' (plus lisibles que des 'V' et des 'F') et sa définition
-//     en extension (ensemble de couples {(..,..),(..,..), ...})
-//     */
-//    public static String toString(){
-//
-//    }
-//
+
+
+    // méthodes
+
+
+    /** pré-req/uis : aucun
+     résultat : une chaîne de caractères permettant d'afficher this par sa matrice d'adjacence
+     contenant des '0' et des '1' (plus lisibles que des 'V' et des 'F') et sa définition
+     en extension (ensemble de couples {(..,..),(..,..), ...})
+     */
+    public String toString(){
+        String s="";
+        for(int i=0;i<this.n;i++){
+            for(int j=0;j<this.n;j++){
+                if (this.matAdj[i][j]){
+                    s+='1';
+                }
+                else s+='0';
+                s+="\t";
+            }
+            s+="\n\n";
+        }
+        s+="\n----------------\n";
+        s+="{";
+        for (int i=0;i<this.n;i++){
+            for(int j=0;j<this.n;j++){
+                if(this.matAdj[i][j]){
+                    s+="("+i+","+j+")";
+                    if(i<this.n-1) {
+                        s += ",";
+                    }
+                }
+            }
+        }
+        s+="}";
+        return s;
+    }
+
 //    //______________________________________________
 //
 //
 //
 //    // A) Logique et calcul matriciel
 //    //-------------------------------
-//
-//
-//    /** pré-requis : m1 et m2 sont des matrices carrées de même dimension et 1 <= numConnecteur <= 5
-//     résultat : la matrice obtenue en appliquant terme à terme le  connecteur de numéro numConnecteur
-//     sur m1 si numConnecteur  = 3 (dans ce cas le paramètre m2 n'est pas utilisé),
-//     et sur m1 et m2 dans cet ordre sinon, sachant que les connecteurs "ou","et","non",
-//     "implique"et "equivalent" sont numérotés de 1 à 5 dans cet ordre
-//     */
-//
-//    public static boolean[][] opBool(boolean[][] m1, boolean[][] m2, int numConnecteur){
-//
-//    }
-//
+
+
+    /** pré-requis : m1 et m2 sont des matrices carrées de même dimension et 1 <= numConnecteur <= 5
+     résultat : la matrice obtenue en appliquant terme à terme le  connecteur de numéro numConnecteur
+     sur m1 si numConnecteur  = 3 (dans ce cas le paramètre m2 n'est pas utilisé),
+     et sur m1 et m2 dans cet ordre sinon, sachant que les connecteurs "ou","et","non",
+     "implique"et "equivalent" sont numérotés de 1 à 5 dans cet ordre
+     */
+
+    public static boolean[][] opBool(boolean[][] m1, boolean[][] m2, int numConnecteur){
+        boolean m[][]=new boolean[m1.length][m1.length];
+        if(numConnecteur==1){
+            for (int i=0;i< m1.length;i++){
+                for (int j=0;j< m1.length;j++){
+                    if(m1[i][j]||m2[i][j])
+                    m[i][j]=true;
+                    else m[i][j]=false;
+                }
+            }
+        }
+        else if(numConnecteur==2){
+            for (int i=0;i< m1.length;i++){
+                for (int j=0;j< m1.length;j++){
+                    if(m1[i][j]&&m2[i][j])
+                        m[i][j]=true;
+                    else m[i][j]=false;
+                }
+            }
+        }
+        else if(numConnecteur==3){
+            for (int i=0;i< m1.length;i++){
+                for (int j=0;j< m1.length;j++){
+                    if(m1[i][j])
+                        m[i][j]=false;
+                    else m[i][j]=true;
+                }
+            }
+        }
+        else if(numConnecteur==4){
+            for (int i=0;i< m1.length;i++){
+                for (int j=0;j< m1.length;j++){
+                    if(!m1[i][j]||m2[i][j])
+                        m[i][j]=true;
+                    else m[i][j]=false;
+                }
+            }
+        }
+        else{
+            for (int i=0;i< m1.length;i++){
+                for (int j=0;j< m1.length;j++){
+                    if(m1[i][j]==m2[i][j])
+                        m[i][j]=true;
+                    else m[i][j]=false;
+                }
+            }
+        }
+        return m;
+    }
+
 //    //______________________________________________
-//
-//
-//    /** pré-requis : m1 et m2 sont des matrices carrées de même dimension
-//     résultat : le produit matriciel de m1 et m2
-//     */
-//    public static boolean[][] produit(boolean[][] m1, boolean[][] m2){
-//
-//    }
-//
+
+
+    /** pré-requis : m1 et m2 sont des matrices carrées de même dimension
+     résultat : le produit matriciel de m1 et m2
+     */
+    public static boolean[][] produit(boolean[][] m1, boolean[][] m2){
+        boolean m[][]=new boolean[m1.length][m1.length];
+        for (int i=0;i< m1.length;i++){
+            for (int j=0;j< m1.length;j++){
+                for (int k=0;k<m1.length; k++){
+                    m[i][j]=m[i][j]||(m1[i][k] && m2[k][j]);
+                }
+            }
+        }
+        return m;
+    }
+
 //    //______________________________________________
-//
-//
-//    /** pré-requis : m est une matrice carrée
-//     résultat : la matrice transposée de m
-//     */
-//    public static boolean[][] transposee(boolean[][] m){
-//
-//    }
-//
+
+
+    /** pré-requis : m est une matrice carrée
+     résultat : la matrice transposée de m
+     */
+    public static boolean[][] transposee(boolean[][] m){
+        boolean[][]t=new boolean[m.length][m.length];
+        for (int i=0;i< m.length;i++){
+            for (int j=0;j< m.length;j++){
+                t[j][i]=m[i][j];
+            }
+        }
+        return t;
+    }
+
 //    //______________________________________________
 //
 //
 //    // B) Théorie des ensembles
 //    //--------------------------
-//
-//
-//    /** pré-requis : aucun
-//     résultat : vrai ssi this est vide
-//     */
-//    public boolean estVide(){
-//
-//    }
-//
+
+
+    /** pré-requis : aucun
+     résultat : vrai ssi this est vide
+     */
+    public boolean estVide(){
+        if (this.m==0)
+            return true;
+        else return false;
+    }
+
 //    //______________________________________________
-//
-//
-//    /** pré-requis : aucun
-//     résultat : vrai ssi this est pleinee (contient tous les couples d'éléments de E)
-//     */
-//    public boolean estPleine(){
-//
-//    }
-//
+
+
+    /** pré-requis : aucun
+     résultat : vrai ssi this est pleinee (contient tous les couples d'éléments de E)
+     */
+    public boolean estPleine(){
+        if(this.m==this.n*this.n)
+            return true;
+        else return false;
+    }
+
 //    //______________________________________________
-//
-//    /** pré-requis : aucun
-//     résultat : vrai ssi (x,y) appartient à this
-//     */
-//    public boolean appartient(int x, int y){
-//
-//    }
-//
+
+    /** pré-requis : aucun
+     résultat : vrai ssi (x,y) appartient à this
+     */
+    public boolean appartient(int x, int y){
+        if(this.matAdj[x][y])
+            return true;
+        else return false;
+    }
+
 //    //______________________________________________
-//
-//
-//    /** pré-requis : 0 <= x < this.n et 0 <= y < this.n
-//     résultat : ajoute (x,y) à this s'il n'y est pas déjà
-//     */
-//    public void ajouteCouple(int x, int y){
-//
-//    }
-//
+
+
+    /** pré-requis : 0 <= x < this.n et 0 <= y < this.n
+     résultat : ajoute (x,y) à this s'il n'y est pas déjà
+     */
+    public void ajouteCouple(int x, int y){
+        if (!this.matAdj[x][y]){
+            this.matAdj[x][y]=true;
+        }
+    }
+
 //    //______________________________________________
-//
-//
-//    /** pré-requis : 0 <= x < this.n et 0 <= y < this.n
-//     résultat : enlève (x,y) de this s'il y est
-//     */
-//    public void enleveCouple(int x, int y){
-//
-//    }
-//
+
+
+    /** pré-requis : 0 <= x < this.n et 0 <= y < this.n
+     résultat : enlève (x,y) de this s'il y est
+     */
+    public void enleveCouple(int x, int y){
+        if (this.matAdj[x][y]){
+            this.matAdj[x][y]=false;
+        }
+    }
+
 //    //______________________________________________
-//
-//
-//    /** pré-requis : aucun
-//     résultat : une nouvelle relation binaire obtenue à partir de this en ajoutant
-//     les couples de la forme  (x,x) qui n'y sont pas déjà
-//     */
-//    public RelationBinaire avecBoucles(){
-//
-//
+
+
+    /** pré-requis : aucun
+     résultat : une nouvelle relation binaire obtenue à partir de this en ajoutant
+     les couples de la forme  (x,x) qui n'y sont pas déjà
+     */
+    public RelationBinaire avecBoucles(){
+        boolean m [][]= copieMat(this.matAdj);
+        for (int i=0;i<this.n;i++){
+            m[i][i]=true;
+        }
+        RelationBinaire R=new RelationBinaire(m);
+        return R;
+    }
+
+
 //        //______________________________________________
-//
-//
-//        /** pré-requis : aucun
-//         résultat : une nouvelle relation binaire obtenue à partir de this en enlèvant
-//         les couples de la forme  (x,x) qui y sont
-//         */
-//        public RelationBinaire sansBoucles(){
-//
-//        }
-//
+
+
+        /** pré-requis : aucun
+         résultat : une nouvelle relation binaire obtenue à partir de this en enlèvant
+         les couples de la forme  (x,x) qui y sont
+         */
+        public RelationBinaire sansBoucles(){
+            boolean m [][]= copieMat(this.matAdj);
+            for (int i=0;i<this.n;i++){
+                m[i][i]=false;
+            }
+            RelationBinaire R=new RelationBinaire(m);
+            return R;
+        }
+
 //        //______________________________________________
-//
-//
-//        /** pré-requis : this.n = r.n
-//         résultat : l'union de this et r
-//         */
-//        public RelationBinaire union(RelationBinaire r){
-//
-//        }
-//
+
+
+
+        /** pré-requis : this.n = r.n
+         résultat : l'union de this et r
+         */
+        public RelationBinaire union(RelationBinaire r){
+            boolean [][] m=new boolean[this.n][this.n];
+            for (int i=0;i<this.n;i++){
+                for (int j=0;j<this.n;j++){
+                    if(this.matAdj[i][j]||r.matAdj[i][j])
+                        m[i][j]=true;
+                }
+            }
+            RelationBinaire R=new RelationBinaire(m);
+            return R;
+        }
+
 //        //______________________________________________
-//
-//
-//        /** pré-requis : this.n = r.n
-//         résultat : l'intersection de this et r
-//         */
-//        public RelationBinaire intersection(RelationBinaire r){
-//
-//        }
-//
+
+
+        /** pré-requis : this.n = r.n
+         résultat : l'intersection de this et r
+         */
+        public RelationBinaire intersection(RelationBinaire r){
+            boolean [][] m=new boolean[this.n][this.n];
+            for (int i=0;i<this.n;i++){
+                for (int j=0;j<this.n;j++){
+                    if(this.matAdj[i][j]&&r.matAdj[i][j])
+                        m[i][j]=true;
+                }
+            }
+            RelationBinaire R=new RelationBinaire(m);
+            return R;
+
+        }
+
 //        //______________________________________________
-//
-//
-//
-//        /** pré-requis : aucun
-//         résultat : la relation complémentaire de this
-//         */
-//        public RelationBinaire complementaire(){
-//
-//        }
-//
+
+
+
+        /** pré-requis : aucun
+         résultat : la relation complémentaire de this
+         */
+        public RelationBinaire complementaire(){
+            boolean [][] m=new boolean[this.n][this.n];
+            for (int i=0;i<this.n;i++){
+                for (int j=0;j<this.n;j++){
+                    if(!this.matAdj[i][j])
+                        m[i][j]=true;
+                }
+            }
+            RelationBinaire R=new RelationBinaire(m);
+            return R;
+        }
+
 //        //______________________________________________
-//
-//
-//        /** pré-requis : this.n = r.n
-//         résultat : la différence de this et r
-//         */
-//        public RelationBinaire difference(RelationBinaire r){
-//
-//
-//        }
-//
+
+
+        /** pré-requis : this.n = r.n
+         résultat : la différence de this et r
+         */
+        public RelationBinaire difference(RelationBinaire r){
+            boolean [][] m=new boolean[this.n][this.n];
+            for (int i=0;i<this.n;i++){
+                for (int j=0;j<this.n;j++){
+                    if(this.matAdj[i][j]!=r.matAdj[i][j])
+                        m[i][j]=true;
+                }
+            }
+            RelationBinaire R=new RelationBinaire(m);
+            return R;
+        }
+
 //        //______________________________________________
-//
-//
-//        /** pré-requis : this.n = r.n
-//         résultat : vrai ssi this est incluse dans r
-//         */
-//        public boolean estIncluse(RelationBinaire r){
-//
-//        }
-//
+
+
+        /** pré-requis : this.n = r.n
+         résultat : vrai ssi this est incluse dans r
+         */
+        public boolean estIncluse(RelationBinaire r){
+            for (int i=0;i<this.n;i++){
+                for (int j=0;j<this.n;j++){
+                    if (this.matAdj[i][j]&&!r.matAdj[i][j]){
+                        return false;
+                    }
+                }
+            }
+            return true;
+
+        }
+
 //        //______________________________________________
-//
-//
-//        /** pré-requis : this.n = r.n
-//         résultat : vrai ssi this est égale à r
-//         */
-//        public boolean estEgale(RelationBinaire r){
-//
-//        }
-//
+
+
+        /** pré-requis : this.n = r.n
+         résultat : vrai ssi this est égale à r
+         */
+        public boolean estEgale(RelationBinaire r){
+            
+        }
+
 //        //______________________________________________
 //
 //
@@ -453,6 +636,6 @@ public class RelationBinaire {
 //         */
 //        public void afficheDivers(){
 //
-//        }
+        }
 
         //______________________________________________
